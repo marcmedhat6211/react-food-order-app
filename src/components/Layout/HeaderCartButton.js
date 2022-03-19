f10829ff34af12a7e6524b936d5120539859894e
Marc Medhat <marcmedhat6211@gmail.com>
2022-03-19 15:58:07 +0200

    adding a cart context

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,31 @@
+import { useContext } from "react";
+
+import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  /**
+   * reduce() method:
+   *    the reduce method takes 2 arguments and returns a single value
+   *        1st argument is a function that takes 2 arguments itself
+   *            1st argument is a value
+   *            2nd argument is the array item in that loop
+   *        2nd argument is the initial value of the 1st argument in the function
+   */
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
