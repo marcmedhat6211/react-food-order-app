896edf49f76e281c9b0b077bdb4dffc6da7e6db9
Marc Medhat <marcmedhat6211@gmail.com>
2022-03-19 16:51:42 +0200

    handling the add to cart logic

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,13 +1,15 @@
+import React from "react";
+
 import classes from "./Input.module.css";
 
 // {...props.input} --> this syntax means that any prop coming from outside will be imported to the input here without me having to add every prop manually
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input {...props.input} />
+      <input ref={ref} {...props.input} />
     </div>
   );
-};
+});
 
 export default Input;
